907f0829ed8cd166198dff729efb623acc06dbe1
PatkaMatka <git@patka.me>
2025-12-31 17:37:02 +0100

    Linked fetcher to names.csv

--- fetcher.py
+++ fetcher.py
@@ -3,8 +3,12 @@ import requests
 versions = requests.get("https://ddragon.leagueoflegends.com/api/versions.json").json()
 data_in = requests.get(f"https://ddragon.leagueoflegends.com/cdn/{versions[0]}/data/en_US/champion.json").json()
 
-print("Name")
 
-for champ in data_in["data"].values():
-    print(champ["name"])
+with open("names.csv", "w") as file:
+    file.write("")
 
+with open("names.csv", "a") as file:
+    file.write("Name\n")
+    for champ in data_in["data"].values():
+        file.write(f"{champ["name"]}\n")
+        print(f"Added {champ["name"]}\n")
