a9e0c7632498b0cbe9a7e9cbfe1f4ef3361b706d
PatkaMatka <git@patka.me>
2026-01-01 18:34:32 +0100

    Tweaked icon_fetcher

--- icon_fetcher.py
+++ icon_fetcher.py
@@ -1,7 +1,12 @@
 import requests
-import os
 from PIL import Image
 from io import BytesIO
+import os
+import shutil
+
+new_folder = "icons"
+shutil.rmtree(new_folder)
+os.makedirs(new_folder)
 
 URL_IMG = f"https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons"
 URL_ID = "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-summary.json"
@@ -9,8 +14,8 @@ URL_ID = "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/gl
 id_in = requests.get(URL_ID).json()
 
 for champion in id_in:
-    print(f"Processing {champion["name"]}"
+    print(f"Processing {champion["name"]}")
     img_in = requests.get(f"{URL_IMG}/{champion["id"]}.png")
     i = Image.open(BytesIO(img_in.content))
-    i.save(f"{champion["name"]}.png")
-    
+    rescaled_i = i.resize((50,50))
+    rescaled_i.save(f"icons/{champion["name"]}.png")
